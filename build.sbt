ea1fc034525decac540515978749b540b1490e1c
jbouffard <jbouffard@azavea.com>
2017-08-23 10:18:18 -0400

    Expanded the example server that shows a FocalMax operation

--- build.sbt
+++ build.sbt
@@ -48,7 +48,10 @@ lazy val maml = crossProject.in(file(".")).
   )
   .jvmSettings(
     libraryDependencies ++= Seq(
-      "org.locationtech.geotrellis" %% "geotrellis-raster" % "1.1.1"
+      "org.locationtech.geotrellis" %% "geotrellis-raster" % "1.1.1",
+      "org.locationtech.geotrellis" %% "geotrellis-spark" % "1.1.1",
+      "org.locationtech.geotrellis" %% "geotrellis-s3" % "1.1.1",
+      "org.apache.spark" %% "spark-core" % "2.0.0"
     )
   )
   .jsSettings()
--- jvm/src/main/scala/serve/MamlService.scala
+++ jvm/src/main/scala/serve/MamlService.scala
@@ -1,12 +1,24 @@
 package maml.serve
 
+import maml.ast._
+import maml.ast.utility._
+import maml.ast.jvm._
+import maml.eval._
+import maml.eval.directive._
+
+import geotrellis.raster._
+import geotrellis.raster.render._
+import geotrellis.spark._
+import geotrellis.spark.io._
+import geotrellis.spark.io.s3._
+
+import cats.data.Validated._
 import akka.actor.ActorSystem
 import akka.event.{LoggingAdapter, Logging}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.client.RequestBuilding
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.http.scaladsl.marshalling._
+import akka.http.scaladsl.model.{HttpResponse, ContentType, HttpEntity, MediaTypes}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling.Unmarshal
@@ -15,25 +27,59 @@ import akka.stream.{ActorMaterializer, Materializer}
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent._
+
 
 object MamlService extends App with Service {
-  implicit val system = ActorSystem()
-  implicit val executor = system.dispatcher
-  implicit val materializer = ActorMaterializer()
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorMaterializer()
 
   val config = ConfigFactory.load()
   val logger = Logging(system, getClass)
 
-  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+  val valueReader = S3ValueReader("geopyspark-test", "srtm")
+  val cMap = ColorRamps.Viridis
+
+  val interpreter = Interpreter.buffering(
+    ScopedDirective.pure[TileLiteral](SourceDirectives.tileLiteralDirective),
+    ScopedDirective.pure[FocalMax](FocalDirectives.focalMaxDirective)
+  )
+
+  Http().bindAndHandle(root, config.getString("http.interface"), config.getInt("http.port"))
 }
 
 trait Service {
-  val routes =
-    logRequestResult("http-test") {
-      pathPrefix("test") {
-        get {
-          complete {
-            "Hello, world"
+  implicit def executor: ExecutionContextExecutor
+  implicit val system: ActorSystem
+  implicit val materializer: Materializer
+
+  def pngAsHttpResponse(png: Png): HttpResponse =
+    HttpResponse(entity = HttpEntity(ContentType(MediaTypes.`image/png`), png.bytes))
+
+  def root =
+    pathPrefix(IntNumber / IntNumber / IntNumber) { (z, x, y) =>
+      rejectEmptyResponse {
+        complete {
+          Future {
+            val tileOpt: Option[Tile] =
+              try {
+                Some(
+                  MamlService.valueReader.reader[SpatialKey, Tile](
+                    LayerId("srtm-test", z)
+                  ).read(x, y)
+                )
+              } catch {
+                case _: ValueNotFoundError => None
+              }
+              tileOpt.flatMap { tile =>
+                MamlService.interpreter(FocalMax(List(TileLiteral(tile)), Square(2)))
+                  .as[Tile]
+                  .toOption
+                  .map { t =>
+                    pngAsHttpResponse(t.renderPng(MamlService.cMap))
+                }
+              }
           }
         }
       }
