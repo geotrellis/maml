7bef6db276c4180886f61d4f59ebceae6a334ef9
Nathan Zimmerman <npzimmerman@gmail.com>
2017-09-25 16:05:26 -0400

    asdf

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,14 +3,6 @@
 set -e
 set -x
 
-mkdir -p "${HOME}/.bintray"
-
-cat <<EOF > "${HOME}/.bintray/.credentials"
-realm = Bintray API Realm
-host = api.bintray.com
-user = $BINTRAY_USER
-password = $BINTRAY_PASS
-EOF
 
 ./sbt "++$TRAVIS_SCALA_VERSION" -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project mamlJVM" publish
 ./sbt "++$TRAVIS_SCALA_VERSION" -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project mamlJS" publish
