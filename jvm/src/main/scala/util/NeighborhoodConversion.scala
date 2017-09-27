dd0b9adfb077506e86018a04b3ed8682eb1578c1
Nathan Zimmerman <npzimmerman@gmail.com>
2017-09-27 14:19:25 -0400

    Add bijection between neighborhood types

--- jvm/src/main/scala/util/NeighborhoodConversion.scala
+++ jvm/src/main/scala/util/NeighborhoodConversion.scala
@@ -11,4 +11,13 @@ object NeighborhoodConversion {
     case Wedge(radius, startAngle, endAngle) => focal.Wedge(radius, startAngle, endAngle)
     case Annulus(innerRadius, outerRadius) => focal.Annulus(innerRadius, outerRadius)
   }
+
+  def apply(mamlN: focal.Neighborhood): Neighborhood = mamlN match {
+    case focal.Square(extent) => Square(extent)
+    case focal.Circle(radius) => Circle(radius)
+    case focal.Nesw(extent) => Nesw(extent)
+    case focal.Wedge(radius, startAngle, endAngle) => Wedge(radius, startAngle, endAngle)
+    case focal.Annulus(innerRadius, outerRadius) => Annulus(innerRadius, outerRadius)
+  }
 }
+
