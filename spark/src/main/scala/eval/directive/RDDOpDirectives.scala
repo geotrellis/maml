bb8ec5c26d8128e38ef8b255cc36333140d2982a
Colin Woodbury <colingw@gmail.com>
2017-10-02 10:21:32 -0700

    (spark) Reinstate sugary `reduce` function

--- spark/src/main/scala/eval/directive/RDDOpDirectives.scala
+++ spark/src/main/scala/eval/directive/RDDOpDirectives.scala
@@ -33,57 +33,70 @@ object RDDOpDirectives {
     TileLayerRDD(fn(rdd1, rdd2), rdd1.metadata.combine(rdd2.metadata))
   }
 
+  /** No, `ri` and `ir` are not the same thing, since order is significant for
+    * subtraction and division.
+    */
+  private def reduce(
+    ri: (RDD[(SpatialKey, Tile)], Int) => RDD[(SpatialKey, Tile)],
+    ir: (Int, RDD[(SpatialKey, Tile)]) => RDD[(SpatialKey, Tile)],
+    rd: (RDD[(SpatialKey, Tile)], Double) => RDD[(SpatialKey, Tile)],
+    dr: (Double, RDD[(SpatialKey, Tile)]) => RDD[(SpatialKey, Tile)],
+    rr: (RDD[(SpatialKey, Tile)], RDD[(SpatialKey, Tile)]) => RDD[(SpatialKey, Tile)],
+    res1: Result,
+    res2: Result
+  ): Result = (res1, res2) match {
+    case (RDDResult(r1), RDDResult(r2)) => RDDResult(binary(rr, r1, r2))
+    case (RDDResult(rdd), IntResult(int)) => RDDResult(rdd.withContext(ri(_, int)))
+    case (IntResult(int), RDDResult(rdd)) => RDDResult(rdd.withContext(ir(int, _)))
+    case (RDDResult(rdd), DoubleResult(double)) => RDDResult(rdd.withContext(rd(_, double)))
+    case (DoubleResult(double), RDDResult(rdd)) => RDDResult(rdd.withContext(dr(double, _)))
+  }
+
+  /* --- FOLDABLE EXPRESSIONS --- */
+
   val addition = Directive { case (a@Addition(_), childResults) =>
-    val grouped: Map[MamlKind, Seq[Result]] = childResults.groupBy(_.kind)
-
-    val scalarSums: Interpreted[Double] =
-      (doubleResults(grouped) |@| intResults(grouped)).map { case (dbls, ints) => dbls.sum + ints.sum }
-
-    (spatialRDDResults(grouped) |@| scalarSums).map { case (rdds, sums) =>
-      val rddSums: TileLayerRDD[SpatialKey] = rdds.reduce { (rdd1, rdd2) => binary({ _ + _ }, rdd1, rdd2) }
-
-      RDDResult(rddSums.withContext(_.localAdd(sums)))
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ + _}, {_ +: _}, {_ + _}, {_ +: _}, {_ + _}, res1, res2)
     }
+
+    Valid(results)
   }
 
   val subtraction = Directive { case (a@Subtraction(_), childResults) =>
     val results: Result = childResults.reduce { (res1, res2) =>
-      (res1, res2) match {
-        case (RDDResult(r1), RDDResult(r2)) => RDDResult(binary({ _ - _ }, r1, r2))
-        case (RDDResult(rdd), IntResult(int)) => RDDResult(rdd.withContext(_ - int))
-        case (IntResult(int), RDDResult(rdd)) => RDDResult(rdd.withContext(_.-:(int)))
-        case (RDDResult(rdd), DoubleResult(double)) => RDDResult(rdd.withContext(_ - double))
-        case (DoubleResult(double), RDDResult(rdd)) => RDDResult(rdd.withContext(_.-:(double)))
-      }
+      reduce({_ - _}, {_ -: _}, {_ - _}, {_ -: _}, {_ - _}, res1, res2)
     }
 
     Valid(results)
   }
 
   val multiplication = Directive { case (a@Multiplication(_), childResults) =>
-    val grouped: Map[MamlKind, Seq[Result]] = childResults.groupBy(_.kind)
-
-    val scalarSums: Interpreted[Double] =
-      (doubleResults(grouped) |@| intResults(grouped)).map { case (dbls, ints) =>
-        dbls.fold(1.0) { (z, i) => z * i } * ints.fold(1) { (z, i) => z * i }
-      }
-
-    (spatialRDDResults(grouped) |@| scalarSums).map { case (rdds, sums) =>
-      val rddSums: TileLayerRDD[SpatialKey] = rdds.reduce { (rdd1, rdd2) => binary({ _ * _ }, rdd1, rdd2) }
-
-      RDDResult(rddSums.withContext(_.localMultiply(sums)))
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ * _}, {_ *: _}, {_ * _}, {_ *: _}, {_ * _}, res1, res2)
     }
+
+    Valid(results)
   }
 
   val division = Directive { case (a@Division(_), childResults) =>
     val results: Result = childResults.reduce { (res1, res2) =>
-      (res1, res2) match {
-        case (RDDResult(r1), RDDResult(r2)) => RDDResult(binary({ _ / _ }, r1, r2))
-        case (RDDResult(rdd), IntResult(int)) => RDDResult(rdd.withContext(_ / int))
-        case (IntResult(int), RDDResult(rdd)) => RDDResult(rdd.withContext(_./:(int)))
-        case (RDDResult(rdd), DoubleResult(double)) => RDDResult(rdd.withContext(_ / double))
-        case (DoubleResult(double), RDDResult(rdd)) => RDDResult(rdd.withContext(_./:(double)))
-      }
+      reduce({_ / _}, {_ /: _}, {_ / _}, {_ /: _}, {_ / _}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val max = Directive { case (Max(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_.localMax(_)}, {(i,r) => r.localMax(i)}, {_.localMax(_)}, {(d,r) => r.localMax(d)}, {_.localMax(_)}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val min = Directive { case (Min(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_.localMin(_)}, {(i,r) => r.localMin(i)}, {_.localMin(_)}, {(d,r) => r.localMin(d)}, {_.localMin(_)}, res1, res2)
     }
 
     Valid(results)
