54a9515d1700332ae28d9f25534d1fabc370a519
Colin Woodbury <colingw@gmail.com>
2017-10-02 14:02:02 -0700

    (spark) All binary ops have directives

--- spark/src/main/scala/eval/directive/RDDOpDirectives.scala
+++ spark/src/main/scala/eval/directive/RDDOpDirectives.scala
@@ -9,8 +9,10 @@ import com.azavea.maml.eval._
 import com.azavea.maml.eval.directive._
 import com.azavea.maml.spark._
 import com.azavea.maml.spark.eval._
-import geotrellis.raster.Tile
+import geotrellis.raster._
+import geotrellis.raster.mapalgebra.{local => gt}
 import geotrellis.spark._
+import geotrellis.vector._
 import org.apache.spark.rdd._
 
 
@@ -97,6 +99,115 @@ object RDDOpDirectives {
   val min = Directive { case (Min(_), childResults) =>
     val results: Result = childResults.reduce { (res1, res2) =>
       reduce({_.localMin(_)}, {(i,r) => r.localMin(i)}, {_.localMin(_)}, {(d,r) => r.localMin(d)}, {_.localMin(_)}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val or = Directive { case (Or(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ | _}, {_ |: _}, {_ | d2i(_)}, {d2i(_) |: _}, {_ | _}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val and = Directive { case (And(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ & _}, {_ &: _}, {_ & d2i(_)}, {d2i(_) &: _}, {_ localAnd _}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val xor = Directive { case (Xor(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce(
+        {_ ^ _},
+        {_ ^: _},
+        {_ ^ d2i(_)},
+        {d2i(_) ^: _},
+        { (r1, r2) => r1.combineValues(r2) { (t1, t2) => t1.combineDouble(t2)(gt.Xor.combine) } },
+        res1,
+        res2
+      )
+    }
+
+    Valid(results)
+  }
+
+  /* --- BINARY EXPRESSIONS --- */
+
+  val equalTo = Directive { case (Equal(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_.localEqual(_)}, {(i,r) => r.localEqual(i)}, {_.localEqual(_)}, {(d,r) => r.localEqual(d)}, {_.localEqual(_)}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val unequalTo = Directive { case (Unequal(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_.localUnequal(_)}, {(i,r) => r.localUnequal(i)}, {_.localUnequal(_)}, {(d,r) => r.localUnequal(d)}, {_.localUnequal(_)}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val lessThan = Directive { case (Less(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ < _}, {_ <<: _}, {_ < _}, {_ <<: _}, {_ < _}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val lessThanOrEqualTo = Directive { case (LessOrEqual(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ <= _}, {_ <=: _}, {_ <= _}, {_ <=: _}, {_ <= _}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val greaterThan = Directive { case (Greater(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ > _}, {_ >>: _}, {_ > _}, {_ >>: _}, {_ > _}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  val greaterThanOrEqualTo = Directive { case (GreaterOrEqual(_), childResults) =>
+    val results: Result = childResults.reduce { (res1, res2) =>
+      reduce({_ >= _}, {_ >=: _}, {_ >= _}, {_ >=: _}, {_ >= _}, res1, res2)
+    }
+
+    Valid(results)
+  }
+
+  // TODO The mask can probably be something other than `Polygon`.
+  val masking = Directive {
+    case (Masking(_), RDDResult(r) :: GeomResult(g: Polygon) :: Nil) => Valid(RDDResult(r.mask(g)))
+    case (Masking(_), GeomResult(g: Polygon) :: RDDResult(r) :: Nil) => Valid(RDDResult(r.mask(g)))
+  }
+
+  val atan2 = Directive { case (Atan2(_), childResults) =>
+    val ri: (TileLayerRDD[SpatialKey], Int) => TileLayerRDD[SpatialKey] = { (r, i) =>
+      r.withContext(_.mapValues(_.mapDouble(math.atan2(i.toDouble, _))))
+    }
+
+    val rd: (TileLayerRDD[SpatialKey], Double) => TileLayerRDD[SpatialKey] = { (r, d) =>
+      r.withContext(_.mapValues(_.mapDouble(math.atan2(d, _))))
+    }
+
+    val results: Result = childResults match {
+      case (RDDResult(r1) :: RDDResult(r2) :: Nil) =>
+        RDDResult(binary({ (a,b) => a.combineValues(b) { (t1,t2) => t1.combineDouble(t2)(math.atan2) }}, r1, r2))
+      case (RDDResult(r) :: IntResult(i) :: Nil) => RDDResult(ri(r, i))
+      case (IntResult(i) :: RDDResult(r) :: Nil) => RDDResult(ri(r, i))
+      case (RDDResult(r) :: DoubleResult(d) :: Nil) => RDDResult(rd(r, d))
+      case (DoubleResult(d) :: RDDResult(r) :: Nil) => RDDResult(rd(r, d))
     }
 
     Valid(results)
