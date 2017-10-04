8f05e4705a7512cf5cac0a62a11327c443df1e92
Nathan Zimmerman <npzimmerman@gmail.com>
2017-10-04 17:10:56 -0400

    Add default naive interpreter

--- jvm/src/main/scala/eval/NaiveInterpreter.scala
+++ jvm/src/main/scala/eval/NaiveInterpreter.scala
@@ -1,6 +1,7 @@
 package com.azavea.maml.eval
 
 import com.azavea.maml.ast._
+import com.azavea.maml.eval.directive._
 
 import cats._
 import cats.implicits._
@@ -25,3 +26,60 @@ case class NaiveInterpreter(directives: List[Directive]) extends Interpreter {
     directives.reduceLeft(_ orElse _).orElse(fallbackDirective)((expression, children))
 }
 
+object NaiveInterpreter {
+
+  def DEFAULT = NaiveInterpreter(
+    List(
+      SourceDirectives.tileLiteral,
+      SourceDirectives.intLiteral,
+      SourceDirectives.dblLiteral,
+      SourceDirectives.boolLiteral,
+      SourceDirectives.geomJson,
+      OpDirectives.additionTile orElse OpDirectives.additionInt orElse OpDirectives.additionDouble,
+      OpDirectives.subtraction,
+      OpDirectives.multiplicationTile orElse OpDirectives.multiplicationInt orElse OpDirectives.multiplicationDouble,
+      OpDirectives.division,
+      OpDirectives.maxTile orElse OpDirectives.maxInt orElse OpDirectives.maxDouble,
+      OpDirectives.minTile orElse OpDirectives.minInt orElse OpDirectives.minDouble,
+      OpDirectives.lessThan,
+      OpDirectives.lessThanOrEqualTo,
+      OpDirectives.equalTo,
+      OpDirectives.notEqualTo,
+      OpDirectives.greaterThan,
+      OpDirectives.greaterThanOrEqualTo,
+      OpDirectives.and,
+      OpDirectives.or,
+      OpDirectives.xor,
+      OpDirectives.masking,
+      OpDirectives.atan2,
+      UnaryDirectives.sin,
+      UnaryDirectives.cos,
+      UnaryDirectives.tan,
+      UnaryDirectives.sinh,
+      UnaryDirectives.cosh,
+      UnaryDirectives.tanh,
+      UnaryDirectives.asin,
+      UnaryDirectives.acos,
+      UnaryDirectives.atan,
+      UnaryDirectives.round,
+      UnaryDirectives.floor,
+      UnaryDirectives.ceil,
+      UnaryDirectives.naturalLog,
+      UnaryDirectives.log10,
+      UnaryDirectives.sqrt,
+      UnaryDirectives.abs,
+      UnaryDirectives.isUndefined,
+      UnaryDirectives.isDefined,
+      UnaryDirectives.numericNegation,
+      UnaryDirectives.logicalNegation,
+      UnaryDirectives.classification,
+      FocalDirectives.max,
+      FocalDirectives.min,
+      FocalDirectives.mean,
+      FocalDirectives.mode,
+      FocalDirectives.median,
+      FocalDirectives.sum,
+      FocalDirectives.standardDeviation
+    )
+  )
+}
--- jvm/src/main/scala/eval/directive/FocalDirectives.scala
+++ jvm/src/main/scala/eval/directive/FocalDirectives.scala
@@ -14,7 +14,7 @@ import Validated._
 
 
 object FocalDirectives {
-  val focalMax = Directive { case (fm@FocalMax(_, neighborhood), childResults) =>
+  val max = Directive { case (fm@FocalMax(_, neighborhood), childResults) =>
     childResults
       .map({ _.as[LazyTile] })
       .toList.sequence
@@ -23,7 +23,7 @@ object FocalDirectives {
       })
   }
 
-  val focalMin = Directive { case (fm@FocalMin(_, neighborhood), childResults) =>
+  val min = Directive { case (fm@FocalMin(_, neighborhood), childResults) =>
     childResults
       .map({ _.as[LazyTile] })
       .toList.sequence
@@ -32,7 +32,7 @@ object FocalDirectives {
       })
   }
 
-  val focalMean = Directive { case (fm@FocalMean(_, neighborhood), childResults) =>
+  val mean = Directive { case (fm@FocalMean(_, neighborhood), childResults) =>
     childResults
       .map({ _.as[LazyTile] })
       .toList.sequence
@@ -41,7 +41,7 @@ object FocalDirectives {
       })
   }
 
-  val focalMedian = Directive { case (fm@FocalMedian(_, neighborhood), childResults) =>
+  val median = Directive { case (fm@FocalMedian(_, neighborhood), childResults) =>
     childResults
       .map({ _.as[LazyTile] })
       .toList.sequence
@@ -50,7 +50,7 @@ object FocalDirectives {
       })
   }
 
-  val focalMode = Directive { case (fm@FocalMode(_, neighborhood), childResults) =>
+  val mode = Directive { case (fm@FocalMode(_, neighborhood), childResults) =>
     childResults
       .map({ _.as[LazyTile] })
       .toList.sequence
@@ -59,7 +59,7 @@ object FocalDirectives {
       })
   }
 
-  val focalSum = Directive { case (fm@FocalSum(_, neighborhood), childResults) =>
+  val sum = Directive { case (fm@FocalSum(_, neighborhood), childResults) =>
     childResults
       .map({ _.as[LazyTile] })
       .toList.sequence
@@ -68,7 +68,7 @@ object FocalDirectives {
       })
   }
 
-  val focalStandardDeviation = Directive { case (fm@FocalStdDev(_, neighborhood), childResults) =>
+  val standardDeviation = Directive { case (fm@FocalStdDev(_, neighborhood), childResults) =>
     childResults
       .map({ _.as[LazyTile] })
       .toList.sequence
