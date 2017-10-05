15bc29c06ea3639054291366a018eeb81eee926d
Nathan Zimmerman <npzimmerman@gmail.com>
2017-10-05 15:33:18 -0400

    Add default RDD interpreter

--- spark/src/main/scala/eval/directive/RDDSourceDirectives.scala
+++ spark/src/main/scala/eval/directive/RDDSourceDirectives.scala
@@ -1,10 +1,12 @@
 package com.azavea.maml.spark.eval.directive
 
-import cats.data._
-import cats.data.Validated._
 import com.azavea.maml.eval.directive._
+import com.azavea.maml.eval._
 import com.azavea.maml.spark.ast._
 import com.azavea.maml.spark.eval._
+
+import cats.data._
+import cats.data.Validated._
 
 
 object RDDSourceDirectives {
