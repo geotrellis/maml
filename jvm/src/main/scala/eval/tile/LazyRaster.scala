e5026f7db912eed3507a8f51383f1e3654e02acb
Eugene Cheipesh <echeipesh@gmail.com>
2019-04-04 18:11:22 -0400

    Revert "Avoid combining raster extents due to floating point error (#82)"
    This reverts commit 77a32d3b0eb3a162eb20e7ae4caf54fdd8491241.

--- jvm/src/main/scala/eval/tile/LazyRaster.scala
+++ jvm/src/main/scala/eval/tile/LazyRaster.scala
@@ -95,11 +95,7 @@ object LazyRaster {
     require(children.length == arity, s"Incorrect arity: $arity argument(s) expected, ${children.length} found")
     def fst = children(0)
     def snd = children(1)
-
-    /** rasterExtent.combine(rasterExtent) was formerly used to derive this value. It sometimes has
-     *   issues with floating point arithmetic. Using the first rasterExtent *should* be fine
-    **/
-    def rasterExtent: RasterExtent = fst.rasterExtent
+    def rasterExtent: RasterExtent = fst.rasterExtent combine snd.rasterExtent
     def crs = fst.crs
   }
 
