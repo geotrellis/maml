4a572effa8708c83afb6b8ba27dde9b9c2b36435
James Santucci <james.santucci@gmail.com>
2019-04-11 18:54:06 -0400

    Remove hillshade

--- jvm/src/main/scala/eval/tile/LazyMultibandRaster.scala
+++ jvm/src/main/scala/eval/tile/LazyMultibandRaster.scala
@@ -77,17 +77,6 @@ case class LazyMultibandRaster(val bands: Map[String, LazyRaster]) {
     LazyMultibandRaster(lztiles)
   }
 
-  def hillshade(
-    gridbounds: Option[GridBounds[Int]],
-    zFactor: Double,
-    cs: CellSize,
-    azimuth: Double,
-    altitude: Double
-  ): LazyMultibandRaster = {
-    val lztiles = bands.mapValues({ lt => LazyRaster.Hillshade(List(lt), gridbounds, zFactor, cs, azimuth, altitude) })
-    LazyMultibandRaster(lztiles)
-  }
-
   def mask(
     maskPoly: MultiPolygon
   ): LazyMultibandRaster = {
