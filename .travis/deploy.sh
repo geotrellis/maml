f87e2da07f311366ebf8d072fbff747edc9bcc9b
Rocky Breslow <rbreslow@azavea.com>
2019-04-23 13:20:56 -0400

    Add STRTA and migrate to CircleCI

--- .travis/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-./sbt "++$TRAVIS_SCALA_VERSION" -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project mamlJVM" publish
-./sbt "++$TRAVIS_SCALA_VERSION" -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project mamlJS" publish
-./sbt "++$TRAVIS_SCALA_VERSION" -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project mamlSpark" publish
-
