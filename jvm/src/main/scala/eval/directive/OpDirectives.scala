1ff68cef8259027e5ea748e3fc2d60c4732607d1
Grigory Pomadchin <gr.pomadchin@gmail.com>
2020-04-08 17:08:10 -0400

    Add Rescale and Normalize nodes

--- jvm/src/main/scala/eval/directive/OpDirectives.scala
+++ jvm/src/main/scala/eval/directive/OpDirectives.scala
@@ -1,10 +1,11 @@
 package com.azavea.maml.eval.directive
 
-import com.azavea.maml.error.{Interpreted, _}
+import com.azavea.maml.error._
 import com.azavea.maml.eval._
 import com.azavea.maml.eval.tile._
 import com.azavea.maml.ast._
 import com.azavea.maml.dsl.tile._
+
 import cats._
 import cats.data._
 import cats.implicits._
